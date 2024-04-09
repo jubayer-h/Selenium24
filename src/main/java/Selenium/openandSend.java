b2037f9b18f29009230f089672dc83d2d525d6a1
jubayerHussain <jubayerh@hotmail.com>
2024-04-09 06:27:09 -0400

    3rd commit opensend java

--- src/main/java/Selenium/openandSend.java
+++ src/main/java/Selenium/openandSend.java
@@ -34,6 +34,7 @@ public class openandSend {
        }
         System.out.println("Fisished is here");
         System.out.println("for send comitt test");
+        System.out.println("for test 3rd commit");
 
 
 
