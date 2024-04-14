a0e0e6541c63370b78b6c9bcccd3bf2609ededb9
jubayer <jubayerh@hotmail.com>
2024-04-13 22:59:18 -0400

    2-Cooking-Notification Handle

--- src/main/java/Selenium/HandlingCooking.java
+++ src/main/java/Selenium/HandlingCooking.java
@@ -22,6 +22,20 @@ public class HandlingCooking {
 
         // Handling cookies:
          driver.manage().deleteAllCookies();
+        //		driver.findElement(By.xpath("//*[@id=\"main-container\"]/div/div[1]/div[3]/div[2]/div[1]/div[2]/a/div/div")).click();
+
+        // Handling radio buttons:
+//		WebElement roundTrip = driver.findElement(By.xpath("//*[@id=\"main-container\"]/div/div[1]/"
+//				+ "div[3]/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/div[1]/svg/circle"));
+//		WebElement oneWay = driver.findElement(By.cssSelector("#main-container > div > div.css-1dbjc4n.r-14lw9ot > "
+//				+ "div.css-1dbjc4n.r-14lw9ot.r-z2wwpe.r-vgw6uq.r-156q2ks.r-11ict49.r-8uuktl.r-136ojw6 > "
+//				+ "div.css-1dbjc4n.r-1pcd2l5.r-1uwte3a.r-m611by.r-bnwqim > div:nth-child(2) > div > "
+//				+ "div.css-1dbjc4n.r-1d09ksm.r-1inuy60.r-1qxgc49 > div:nth-child(2) > div > div:nth-child(1) > "
+//				+ "div.css-1dbjc4n.r-zso239 > svg > g > circle:nth-child(1)"));
+//		Thread.sleep(5000);
+//		roundTrip.click();
+//		Thread.sleep(5000);
+//		oneWay.click();
          driver.close();
 
     }
